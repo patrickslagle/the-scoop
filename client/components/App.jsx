4436a216019b4233492d009777d860231c5618ce
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-02 19:43:16 -0800

    made app stateless component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Mapbox from './Mapbox.jsx';
 import Header from './Header.jsx';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render(){
-    return(
-      <div className="app-container">
-        <Header/>
-        <Mapbox/>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="app-container">
+    <Header />
+    <Mapbox />
+  </div>
+);
 
 export default App;
