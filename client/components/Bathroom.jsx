b69c660b2f2e50deb2fc4b9f6c198d61f0af38f5
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-02 19:42:17 -0800

    linting file

--- client/components/Bathroom.jsx
+++ client/components/Bathroom.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactMapboxGl, { Marker } from "react-mapbox-gl";
+import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
 import styled from 'styled-components';
 
 
