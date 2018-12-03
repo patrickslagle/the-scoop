08946cff3a222127cddd7ad252d0ea84087719cd
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-02 19:50:00 -0800

    linted header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const Header = props => {
-  return (
+const Header = () => (
   <div>
     <header>
       <h1>The Poop Scoop 💩</h1>
     </header>
     <div className="header-pic">
-      <img src="https://outsider.ie/wp-content/uploads/2017/08/ALT-HEADER-3401601513_80ea646cd9_o-1980x450.jpg"></img>
+      <img
+        src="https://outsider.ie/wp-content/uploads/2017/08/ALT-HEADER-3401601513_80ea646cd9_o-1980x450.jpg" 
+        alt="get the scoop on where to poop"
+      />
     </div>
   </div>
-)};
+);
 
-export default Header; 
+export default Header;
