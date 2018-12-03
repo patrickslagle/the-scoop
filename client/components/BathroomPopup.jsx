877635b00cbcf46f3c023c4fd776913ec5802491
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-02 21:10:11 -0800

    linted BathroomPopup

--- client/components/BathroomPopup.jsx
+++ client/components/BathroomPopup.jsx
@@ -2,40 +2,53 @@ import React from 'react';
 import ReactMapboxGl, { Popup } from "react-mapbox-gl";
 
 
-const BathroomPopup = props => {
-  return (
+const BathroomPopup = ({
+  coordinate,
+  submitHandler,
+  bathroomLocation,
+  bathroomPic,
+  handleChange,
+  review
+}) => (
   <div>
-    <Popup className="popup"
-      coordinates={props.coordinate}
-      // onClick={() => this.addLike({test: 'testing'})}
+    <Popup
+      className="popup"
+      coordinates={coordinate}
       anchor="bottom"
       offset={{
-        'bottom-left': [0, 0],  'bottom': [0, 0], 'bottom-right': [0, 0]
-      }}>
-    <h1>Add Bathroom</h1>
-    <form onSubmit={props.submitHandler}> 
-      <input type="text"
-        placeholder="Bathroom Location"
-        value={props.bathroomLocation} 
-        onChange={props.handleChange} 
-      /><br/>
-      <input type="text"
-        placeholder="Bathroom URL Pic"
-        value={props.bathroomPic} 
-        onChange={props.handleChange}  
-      /><br/>
-      <input type="text"
-        placeholder="Poop Review"
-        value={props.review} 
-        onChange={props.handleChange}  
-      /><br/>
-      <input type="submit" 
-        value="Submit"
-      />
-    </form>
-  </Popup>
+        'bottom-left': [0, 0],  'bottom': [0, 0], 'bottom-right': [0, 0],
+      }}
+    >
+      <h1>Add Bathroom</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Bathroom Location"
+          value={bathroomLocation}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Bathroom URL Pic"
+          value={bathroomPic}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Poop Review"
+          value={review}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </Popup>
   </div>
-)};
+);
 
-export default BathroomPopup; 
-
+export default BathroomPopup;
